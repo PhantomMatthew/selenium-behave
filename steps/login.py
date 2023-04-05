93b2f0bda89b4ccce3e2e46f0c126a79f8b17efb
phantommatthew <jiabei.yuan95@gmail.com>
2023-04-05 21:54:45 +0800

    update step code to step_impl

--- steps/login.py
+++ steps/login.py
@@ -5,21 +5,21 @@ from pages.login import login_page
 
 
 @when(u'I input username "{username}"')
-def input_username(context, username):
+def step_impl(context, username):
     login_page.input_username(username)
 
 @when(u'I input captcha "{captcha}"')
-def input_captcha(context, captcha):
+def step_impl(context, captcha):
     login_page.input_captcha(captcha)
 
 @when(u'I input validation code "{validation_code}"')
-def input_validation_code(context, validation_code):
+def step_impl(context, validation_code):
     login_page.input_validation_code(validation_code)
 
 @when(u'I press the login button')
-def click_login_button(context):
+def step_impl(context):
     login_page.login()
 
 @then(u'I should see content "{content}" in the results')
-def results(context, content):
+def step_impl(context, content):
     login_page.verify_login_results(content)
